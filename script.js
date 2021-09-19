bffd8d81d0518972520cbf8d881939561461e44d
arghyachowdhury2610@gmail.com <arghya@gintaa.com>
2021-09-19 23:04:53 +0530

    Initial

---
+++ script.js
@@ -0,0 +1,18 @@
+const section = document.querySelector("body");
+let currentPixel = window.pageYOffset
+
+//looper keeps running and keeps track of where the new pixel is
+const looper = function () {
+    const newPixel = window.pageYOffset;
+    const diff = newPixel - currentPixel
+    const speed = diff * 0.35;
+
+    section.style.transform = "skewY(" + speed + "deg)"
+
+
+    currentPixel = newPixel;
+
+    requestAnimationFrame(looper)
+}
+
+looper();
